a8e879e8ab776ccdfae7fd4146cc005a9ec15b74
Cristian Molina <cmutep@gmail.com>
2020-11-12 02:04:17 -0700

    part1 works now

---
+++ lab4/topos/part1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
+from mininet.node import RemoteController
+
+
+class part1_topo(Topo):
+    
+    def build(self):
+        
+        # Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+        s1 = self.addSwitch( 's1' )
+
+
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s1 )
+        self.addLink( h4, s1 )
+
+topos = {'part1' : part1_topo}
+
+if __name__ == '__main__':
+    t = part1_topo()
+    net = Mininet (topo=t)
+    net.start()
+    CLI(net)
+    net.stop()
